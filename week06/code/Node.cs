8fb06e5d17598ecaecd6795aaea05f3b9b8178fe
John Solomon <jaywisefreeworld59@gmail.com>
2026-02-16 18:37:45 +0100

    Trees assignment completed

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,8 +12,13 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        if (value == Data)
+        {
+        // Do nothing — no duplicates allowed
+            return;
+        }
 
-        if (value < Data)
+        else if (value < Data)
         {
             // Insert to the left
             if (Left is null)
@@ -21,7 +26,7 @@ public class Node
             else
                 Left.Insert(value);
         }
-        else
+        else 
         {
             // Insert to the right
             if (Right is null)
@@ -34,12 +39,32 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+        else if (value < Data)
+        {
+            if (Left is null)
+                return false;
+            else
+                return Left.Contains(value);
+        }
+        else
+        {
+            if (Right is null)
+                return false;
+            else
+                return Right.Contains(value);
+        }
+       
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+
+        return 1 + Math.Max(leftHeight, rightHeight);
+        
     }
 }
